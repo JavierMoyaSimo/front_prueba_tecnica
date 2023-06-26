df08c8419f8bf9546a47019c656907fad492f125
Javi <javiermoyasimo@gmail.com>
2023-06-26 15:31:18 +0200

    feat: First commit. Init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <HelloWorld />
+  <TheWelcome />
+</template>
+
+<style scoped></style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,8 @@
+<script setup>
+//Aqui van los imports
+</script>
+
+<template>
+ 
+ <h2>ADIOS JAVI</h2>
+</template>
