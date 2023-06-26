b9f09db8a807184f055f165d19b90ecdbbd2157d
Javi <javiermoyasimo@gmail.com>
2023-06-26 21:00:07 +0200

    feat: Comments deleted

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -47,38 +47,3 @@ h1 {
 
 
 
-<!-- <script setup>
-const goRegister = () => {
-
-}
-</script>
-
-<template>
-  <div class="home-div">
-    <h1>HOME</h1>
-    <div class="buttons-div">
-      <button @click="goRegister">REGISTER</button>
-      <button>LOGIN</button>
-    </div>
-  </div>
-</template>
-
-<style scoped>
-.home-div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-h1 {
-  color: green;
-}
-
-.buttons-div {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100%;
-}
-</style> -->
