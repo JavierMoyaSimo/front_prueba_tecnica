ad1f7367948e6f725a16bd5c75a8760f7597f722
Javi <javiermoyasimo@gmail.com>
2023-06-26 20:58:29 +0200

    feat: Buttons in home.vue added. Buttons now navigate to Register and Login correctly

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,53 @@
 <script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const goRegister = () => {
+  router.push('/register');
+};
+
+const goLogin = () => {
+  router.push('/login');
+};
+</script>
+
+<template>
+  <div class="home-div">
+    <h1>HOME</h1>
+    <div class="buttons-div">
+      <button @click="goRegister">REGISTER</button>
+      <button @click="goLogin">LOGIN</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+h1 {
+  color: green;
+}
+
+.buttons-div {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+}
+</style>
+
+
+
+
+
+<!-- <script setup>
 const goRegister = () => {
 
 }
@@ -32,4 +81,4 @@ h1 {
   align-items: center;
   width: 100%;
 }
-</style>
+</style> -->
